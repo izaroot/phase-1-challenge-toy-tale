aded02f540fc885e58fbd61da55f583b91921002
Isaac <izaroot327@gmail.com>
2021-04-14 10:39:01 -0400

    Added GET, POST and added to DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 let addToy = false;
 
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollectionDiv = document.querySelector("#toy-collection")
+  const toyForm = document.querySelector("form.add-toy-form")
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +16,63 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function turnToyObjToHTML(toyPOJO){
+    let newDiv = document.createElement("div")
+    newDiv.className = "card"
+    
+    let newH2 = document.createElement("h2")
+    newH2.innerText = toyPOJO.name
+
+    let newImg = document.createElement("img")
+    newImg.className = "toy-avatar"
+    newImg.src = toyPOJO.image
+
+    let newP = document.createElement("p")
+    newP.innerText = toyPOJO.likes
+
+    let newBtn = document.createElement("button")
+    newBtn.className = "like-btn"
+    newBtn.id = toyPOJO.id
+    newBtn.innerText = "Like <3"
+
+    newDiv.append(newH2, newImg, newP, newBtn)
+
+    toyCollectionDiv.append(newDiv)
+  }
+
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(json => {
+    console.log(json)
+    json.forEach(toyObj => {
+      turnToyObjToHTML(toyObj)
+    })
+  })
+
+  toyForm.addEventListener('submit', function(evt){
+    evt.preventDefault()
+
+    let toyName = evt.target.name.value
+    let toyImage = evt.target.image.value
+
+    console.log(toyName, toyImage)
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        name: toyName,
+        image: toyImage,
+        likes: 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(newToy => {
+      turnToyObjToHTML(newToy)
+    })
+  })
+
 });
